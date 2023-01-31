d51464638df8e6b0343d7286027841604a38aca9
zaneeeee <123951606+zaneeeee@users.noreply.github.com>
2023-02-01 07:49:26 +1100

    randomise the order of questions and options

--- wwtbam.py
+++ wwtbam.py
@@ -1,4 +1,6 @@
 # Who wants to be a millionaire terminal game
+import random
+
 # Class to store player information such as name and score
 class Player:
     def __init__(self, name):
@@ -8,29 +10,39 @@ class Player:
     def update_score(self, score):
         self.score = score
 
-# Class to store question information such as question number, question, options, and answer
-class Question:
-    def __init__(self, number, question, options, answer):
-        self.number = number
-        self.question = question
-        self.options = options
-        self.answer = answer
+# Define questions, options, answers
+questions = [
+    {
+        'question': 'What is the capital of France?',
+        'options': ['London', 'Paris', 'Berlin', 'Madrid'],
+        'answer': 'Paris'
+    },
+    {
+        'question': 'What is the currency of Japan?',
+        'options': ['Yen', 'Dollar', 'Euro', 'Pound'],
+        'answer': 'Yen'
+    },
+    {
+        'question': 'What is the tallest mountain in the world?',
+        'options': ['Mount Kilimanjaro', 'Mount Everest', 'K2', 'Mount Blanc'],
+        'answer': 'Mount Everest'
+    },
+    {
+        'question': 'What is the largest ocean in the world?',
+        'options': ['Indian Ocean', 'Atlantic Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+        'answer': 'Pacific Ocean'
+    }
+]
 
-# Define the question_value list to hold the value of each question
 question_value = [500, 1000, 2000, 5000, 10000, 20000, 50000, 75000, 1500000, 200000, 500000, 1000000]
 
 # Class to handle the game
 class Game:
     def __init__(self):
-        self.questions = [
-            Question(1, "What is the capital of France?", ["A. Paris", "B. London", "C. Berlin", "D. Madrid"], "A"),
-            Question(2, "What is the currency of Japan?", ["A. Dollar", "B. Euro", "C. Yen", "D. Pound"], "C"),
-            Question(3, "What is the largest ocean in the world?", ["A. Atlantic", "B. Indian", "C. Arctic", "D. Pacific"], "D"),
-            Question(4, "Who painted the Mona Lisa?", ["A. Michelangelo", "B. Leonardo da Vinci", "C. Raphael", "D.Donatello"], "B")
-            ]
+        self.questions = questions
 
-    # Welcome message for the game
-    def welcome_message(self):
+    # Title for the game
+    def title(self):
         print("""
 WHO WANTS TO BE A...
   __  __ _____ _      _      _____ ____  _   _          _____  ______ ___  
@@ -41,38 +53,39 @@ WHO WANTS TO BE A...
  |_|  |_|_____|______|______|_____\____/|_| \_/_/    \_\_|  \_\______|(_)  
 
         """)
-        print("""
-Welcome to WWTBAM! You will be asked a series of multiple-choice questions that each have four 
-possible answers (A,B,C,D). You must answer all of these questions correctly, one at a time, in 
-order to win the million dollars. As soon as you answer a question incorrectly, the game is over.
-""")
 
     # Main method to run game
     def play(self):
-        self.welcome_message()
-        # Get the player's name and instantiate the player object
-        player_name = input("Please enter your name: ")
-        player = Player(player_name)
+        random.shuffle(self.questions) #randomize the order of questions
+        self.title() #print title
+        player_name = input("Please enter your name: ") #get the player's name
+        player = Player(player_name) #instantiate the player object
+        print(f"\nWelcome to WWTBAM {player.name}!") #welcome message for game
 
         # Loop through questions list
-        for question in self.questions:
+        for index, question in enumerate(self.questions):
             print()
-            print(f"Question {question.number}:") #print the question number
-            print(question.question) #print the question
-            print(*question.options, sep="\n") #print the options
+            print(f"Question {index + 1}:") #print the question number
+            print(f"{question['question']}") #print the question
+
+            # Randomise the order of options
+            self.options = question['options']
+            random.shuffle(self.options)
+            for j, option in enumerate(self.options):
+                print(f"{chr(97 + j).upper()}. {option}") #print the options
 
             # Check to ensure the player's input is 'A', 'B', 'C', or 'D'
             while True:
-                answer = input("Your answer: ").upper()
-                if answer in ['A', 'B', 'C', 'D']:
+                self.answer = input("Your answer: ").upper()
+                if self.answer in ["A", "B", "C", "D"]:
                     break
                 else:
                     print("Invalid input. Please enter a valid answer (A, B, C, or D)")
 
             # If the answer is correct, update the player's score and print "Correct!"
-            if answer == question.answer:
-                print("\nCorrect!")
-                player.update_score(question_value[question.number - 1])
+            if self.answer == chr(97 + self.options.index(question['answer'])).upper():
+                print("\nCorrect!") 
+                player.update_score(question_value[index])
             else:
                 # If the answer is incorrect, print "Incorrect!" and an exit message
                 print("\nIncorrect!")
