2659cb8eb13ea48f338efd045bf9328d9732b8df
zaneeeee <123951606+zaneeeee@users.noreply.github.com>
2023-02-18 10:38:02 +0000

    import questions.py

--- wwtbam.py
+++ wwtbam.py
@@ -1,5 +1,8 @@
-# Who wants to be a millionaire terminal game
+"""Who Wants to Be a Millionaire terminal game.
+This module implements a simple terminal-based version of the game "Who Wants to Be a Millionaire".
+"""
 import random
+from questions import QUESTIONS
 
 # Define constant variables
 ANSWER_OPTIONS = ['a', 'b', 'c', 'd']
@@ -14,68 +17,6 @@ WHO WANTS TO BE A...
 ██║ ╚═╝ ██║██║███████╗███████╗██║╚██████╔╝██║ ╚████║██║  ██║██║██║  ██║███████╗  ██╗   
 ╚═╝     ╚═╝╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝  ╚═╝
     """
-QUESTIONS = [
-    {
-        'question': 'What is the capital of France?',
-        'options': ['London', 'Paris', 'Berlin', 'Madrid'],
-        'answer': 'Paris'
-    },
-    {
-        'question': 'What is the currency of Japan?',
-        'options': ['Yen', 'Dollar', 'Euro', 'Pound'],
-        'answer': 'Yen'
-    },
-    {
-        'question': 'What is the tallest mountain in the world?',
-        'options': ['Mount Kilimanjaro', 'Mount Everest', 'K2', 'Mount Blanc'],
-        'answer': 'Mount Everest'
-    },
-    {
-        'question': 'What is the largest ocean in the world?',
-        'options': ['Indian Ocean', 'Atlantic Ocean', 'Arctic Ocean', 'Pacific Ocean'],
-        'answer': 'Pacific Ocean'
-    },
-    {
-        'question': 'Who is the current President of the United States?',
-        'options': ['Barack Obama', 'Donald Trump', 'Joe Biden', 'George W. Bush'],
-        'answer': 'Joe Biden'
-    },
-    {
-        'question': 'What is the largest desert in the world?',
-        'options': ['Antarctica', 'Sahara Desert', 'Arabian Desert', 'Gobi Desert'],
-        'answer': 'Sahara Desert'
-    },
-    {
-        'question': 'What is the longest river in the world?',
-        'options': ['Nile River', 'Amazon River', 'Yangtze River', 'Yellow River'],
-        'answer': 'Nile River'
-    },
-    {
-        'question': 'Who is the author of the Harry Potter series?',
-        'options': ['J.K. Rowling', 'Stephen King', 'John Green', 'Suzanne Collins'],
-        'answer': 'J.K. Rowling'
-    },
-    {
-        'question': 'Who painted the famous artwork "The Starry Night"?',
-        'options': ['Vincent van Gogh', 'Pablo Picasso', 'Rembrandt', 'Claude Monet'],
-        'answer': 'Vincent van Gogh'
-    },
-    {
-        'question': 'What is the chemical symbol for gold?',
-        'options': ['Au', 'Ag', 'Fe', 'Al'],
-        'answer': 'Au'
-    },
-    {
-        'question': 'Who was the first man to walk on the moon?',
-        'options': ['Neil Armstrong', 'Buzz Aldrin', 'Yuri Gagarin', 'John Glenn'],
-        'answer': 'Neil Armstrong'
-    },
-    {
-        'question': 'Who discovered America?',
-        'options': ['Christopher Columbus', 'Leif Erikson', 'Marco Polo', 'Vasco da Gama'],
-        'answer': 'Christopher Columbus'
-    }
-]
 
 # Class to store player information such as name and score
 class Player:
